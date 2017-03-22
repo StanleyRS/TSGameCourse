80430c7cbc25951109aa4929b591194a52e2ae85
StanleyRS <stask0_0@abv.bg>
2017-03-22 23:48:09 +0200

    Add files via upload

---
+++ TSGameCourse/TSGameCourse/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSGameCourse
+{
+    public partial class Form1 : Form
+    {
+        int _x;
+        int _y;
+        
+        int BallWidth = 50;
+        int BallHeight = 50;
+
+        Random rnd = new Random();
+
+        int startPos;
+
+        int speed = 7;
+        bool away = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _x = rnd.Next(panel1.Width - BallWidth);
+            _y = rnd.Next(panel1.Height / 2);
+
+            startPos = _y + BallHeight;
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.DeepSkyBlue, _x, _y, BallWidth, BallHeight);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myTimer.Start();
+        }
+
+        private void myTimer_Tick(object sender, EventArgs e)
+        {
+            if (away)
+            {
+                _y += speed;
+
+                if (_y >= panel1.Height - BallHeight)
+                {
+                    myTimer.Interval = 65;
+
+                    _y -= speed;
+
+                    away = false;
+                }
+            }
+            else
+            {
+                _y -= speed;
+
+                if (_y <= startPos)
+                {
+                    myTimer.Stop();
+
+                    this.Refresh();
+                
+                }
+            }
+
+            this.Refresh();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ TSGameCourse/TSGameCourse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSGameCourse
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        
+        /*
+         чертаем панел
+         чертаем запълнен кръс с фунция за чертаене на елипса
+         появява се на произволна точка
+         създаваме 2 бутона старт и стоп
+         при старт кръга се движи надоло
+         при достигане на долна част на панела, отскача обратно с по-ниска скорост */
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
